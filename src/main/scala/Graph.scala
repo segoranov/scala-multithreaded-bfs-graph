a0578e4c7a2c4d32b70b391523d66592b0c7c122
stiliyan <stiliqn.goranov@gmail.com>
2019-04-27 16:23:13 +0300

    create basic working unit tests

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,9 +1,32 @@
 package graph
 
-case class Graph(numVertices: Int) {
-  def test = true
+import graph.Graph._
+
+case class Graph(adjMatrix: AdjMatrix) {
+
+  def getVertices = List.range(0, adjMatrix.size)
+
+  def hasVertex(v: Vertex) = 0 <= v && v <= adjMatrix.size - 1
+  def isEdge(v1: Vertex, v2: Vertex) = hasVertex(v1) && hasVertex(v2) && adjMatrix(v1)(v2) == 1
+
 
   // def bfsTraversal(start: Int, end: Int, neighbours: Int => List[Int]): Queue = ???
+}
+
+case object Graph {
+  type Row = List[Int]
+  def Row(xs: Int*) = List(xs: _*)
+
+  type AdjMatrix = List[Row]
+  def AdjMatrix(xs: Row*) = List(xs: _*)
+
+  type Vertex = Int
+}
+
+object MyTest {
+  def main(args: Array[String]): Unit = {
+    
+  }
 }
 
 
--- src/test/scala/GraphTest.scala
+++ src/test/scala/GraphTest.scala
@@ -1,16 +1,47 @@
 package graph
 
+import graph.Graph.{AdjMatrix, Row}
 import org.scalatest.{FlatSpec, Matchers}
 
 class GraphTest extends FlatSpec with Matchers {
-  "test" should "be always true" in {
-    val g = Graph(5)
-    g.test shouldBe true
+
+  val testGraph = Graph(AdjMatrix(  Row(0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0),
+                                    Row(1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+                                    Row(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                                    Row(1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0),
+                                    Row(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+                                    Row(1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1),
+                                    Row(0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0),
+                                    Row(0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+                                    Row(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
+                                    Row(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
+                                    Row(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+                                  ))
+
+  "all vertices" should "be from 0 to 10" in {
+    testGraph.getVertices shouldBe List.range(0, 11)
+    testGraph.getVertices.size shouldBe 11
   }
 
-  it should "be always true in" in {
-    val g = Graph(5)
-    g.test shouldBe true
+  "vertices" should "be in graph" in {
+    List.range(0, 11).foreach(vertex => testGraph.hasVertex(vertex) shouldBe true)
   }
 
+  "vertices" should "not be in graph" in {
+    List.range(-10, 0).foreach(vertex => testGraph.hasVertex(vertex) shouldBe false)
+    List.range(11, 30).foreach(vertex => testGraph.hasVertex(vertex) shouldBe false)
+  }
+
+  "edges" should "either exists or not exist" in {
+
+    testGraph.isEdge(0,1) shouldBe true
+    testGraph.isEdge(0,0) shouldBe false
+
+    testGraph.getVertices.foreach(vertex => {
+      if (vertex == 5)
+        testGraph.isEdge(10, vertex) shouldBe true
+      else
+        testGraph.isEdge(10, vertex) shouldBe false
+    })
+  }
 }
