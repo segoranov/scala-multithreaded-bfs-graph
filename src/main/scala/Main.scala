3fa3dbca8a5a6898f6246c401e00c533e5644d34
stiliyan <stiliqn.goranov@gmail.com>
2019-05-15 23:10:02 +0300

    remove unnecessary code and comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,8 @@
 package graph
 
-import graph.Graph.{AdjMatrix, Row}
-
 object Test {
 
   def main(args: Array[String]): Unit = {
-    val testGraph = Graph(AdjMatrix(
-      Row(0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0),
-      Row(1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
-      Row(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-      Row(1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0),
-      Row(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
-      Row(1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1),
-      Row(0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0),
-      Row(0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
-      Row(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
-      Row(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
-      Row(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
-    ))
 
     val testGraphManyVertices = Graph.withRandomEdges(numberOfVertices = 200)
 
@@ -33,19 +18,9 @@ object Test {
     testGraphManyVertices.bfsTraversalStartingFromAllVertices(numberOfTasks = 40)
     testGraphManyVertices.bfsTraversalStartingFromAllVertices(numberOfTasks = 80)
 
-
-    // QUESTIONS:
-    // 0. Is my logic correct: If all threads in the pool are busy,
-    // Futures (tasks) will wait in a queue for the thread pool to free some thread.
-
-    // 1. Why the fuck with 100 tasks is faster than with 4 tasks???
-
-    // 2. How come when I start 100 tasks, the program uses 100 threads, when I have 4 cores?
-
-    // 3. Why is the program not terminating - probably some thread is still running, but which one???
-
     // TODO:
     //  Logging to a file - thread stars, thread finishes, etc...
-    //  Command line parameters
+    //  Command line parameters - see requirements
+    //  Documentation - detailed and desriptive!
   }
 }
