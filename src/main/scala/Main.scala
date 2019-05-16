704d7521b35c5f7e69ee8d05d9592559b8391aae
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2019-05-16 11:49:09 +0300

    start work on command line arguments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,21 @@
 package graph
 
-object Test {
+object GraphApp {
+
+  val usage = """
+    Usage: java -jar <file.jar> -i <graph-file.in> -t <number of tasks> -o <graph-data.out> -q
+           java -jar <file.jar> -n <number_of_vertices> -t <number of tasks> -o <graph-data.out> -q
+
+    The parameters -i and -n are mutually exclusive. Only one of the two should be present.
+    The parameter -o is optional - if not present, no results will be written to a file.
+    The parameter -q (quiet) is optional - if present, only the total time for the BFS will be written to the output file.
+  """
 
   def main(args: Array[String]): Unit = {
+
+    if (args.isEmpty) {
+      println(usage)
+    }
 
     val testGraphManyVertices = Graph.withRandomEdges(numberOfVertices = 300)
 
